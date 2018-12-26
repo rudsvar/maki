fd108e5dd7cbd357b2efe5e575f78ac31c13a399
Rudi B. Svartveit <rudsvar@gmail.com>
2018-12-26 22:10:01 +0100

    Finish updating default.nix

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "rudsvar";
     repo = "maki";
-    rev = "613af954cb0679aeb0460a33a1a18e4cc5a0f184";
+    rev = "83e994fabdd7d6c3cc6e70d250d5469449754154";
     sha256 = "1gxm0k16q1lksbvgzzmcl1g6xm3dyb2n89cgpjci9jpr07igd6s0";
   };
   buildPhase = ''
